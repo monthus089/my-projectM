baffdcb539379693cbf0f41126c39fd9f99322d9
monthus089 <91974247+monthus089@users.noreply.github.com>
2023-03-08 21:35:47 +0700

    8/3/23 -3

--- src/AdminApp.js
+++ src/AdminApp.js
@@ -20,9 +20,9 @@ function AdminApp() {
     ];
 
     return (
-        <div className='relative w-max'>
+        <div className='flex w-max'>
             <div className={`relative bg-[#942bc5] h-screen p-5 pt-8 duration-300 z-0 overflow-hidden ${open ? "w-96" : "w-20"}`}>
-                <BsArrowLeftShort className={`absolute bg-white text-black text-3xl top-1/2 bottom-1/2  border border-white rounded-[9999px] cursor-pointer -right-[-10px] ${!open && "rotate-180"} `} onClick={() => setOpen(!open) && ""}></BsArrowLeftShort>
+                {/* <BsArrowLeftShort className={`absolute bg-white text-black text-3xl top-1/2 bottom-1/2  border border-white rounded-[9999px] cursor-pointer -right-[-50px] ${!open && "rotate-180"} `} onClick={() => setOpen(!open) && ""}></BsArrowLeftShort> */}
                 <div className='inline-flex pb-5'>
                     <img src={logo} alt='' className={`bg-none text-4xl rounded cursor-pointer block float-left mr-2 leading-[60px]`}></img>
                     <h1 className={`text-white origin-left font-medium  text-2xl duration-300 truncate ${!open && "scale-0"}`}>ProjectManagement</h1>
@@ -30,7 +30,7 @@ function AdminApp() {
                 <ul className='pt-2'>
                     {Menu.map((menu, index) => (
                         <>
-                            <li key={index} className={`flex text-white text-sm item-center gap-x-4 cursor-pointer p-2 hover:text-black hover:bg-slate-200 rounded-[25px]  ${menu.spacing ? "mt-[560px]" : "mt-2"}`}>
+                            <li key={index} className={`flex text-white text-sm item-center gap-x-4 cursor-pointer p-4 mr-14 hover:text-black hover:bg-slate-200 rounded-[25px] last:bg-slate-200 last:text-black ${menu.spacing ? "mt-[500px]" : "mt-2"}`}>
                                 <span className='text-xl block float-left '>
                                     {menu.icon}
                                 </span>
@@ -46,7 +46,7 @@ function AdminApp() {
 
 
             {/* Page */}
-            <div className='bg-white  min-h-screen '>
+            <div className='absolute p-0 left-[335px] h-screen w-full border rounded-l-extent bg-white'>
                 {
 
                 }
