c50076a4ddcec65d5b7752d7467277ecf716002b
monthus089 <91974247+monthus089@users.noreply.github.com>
2023-04-06 20:16:40 +0700

    6/4/23 Complete in Require

--- src/components/Student/CreateProgress.js
+++ src/components/Student/CreateProgress.js
@@ -31,6 +31,60 @@ const CreateProgress = () => {
                             </div>
                         </div>
                     </div>
+                    <div className="items-center text-start my-10 whitespace-nowrap">
+                        <div className="pl-6 py-5"><span>Progress Summary</span></div>
+                        <div>
+                            <textarea
+                                name=""
+                                id="" placeholder="Progress Summary..."
+                                className="px-4 pt-[0.35rem] ml-[50px] mt-[10px] text-[20px] w-[90%] h-[120px] block text-gray-900 bg-gray-50 rounded-[18px] border border-gray-300 resize-none scrollbar-hide focus:outline-none"
+                            ></textarea>
+                        </div>
+                    </div>
+
+                    <div className="items-center text-start my-10 whitespace-nowrap">
+                        <div className="pl-6 py-5"><span>Improvements</span></div>
+                        <div>
+                            <textarea
+                                name=""
+                                id="" placeholder="Improvements..."
+                                className="px-4 pt-[0.35rem] ml-[50px] mt-[10px] text-[20px] w-[90%] h-[120px] block text-gray-900 bg-gray-50 rounded-[18px] border border-gray-300 resize-none scrollbar-hide focus:outline-none"
+                            ></textarea>
+                        </div>
+                    </div>
+                    <div className="items-center text-start my-10 whitespace-nowrap">
+                        <div className="pl-6 py-5"><span>Next Goal</span></div>
+                        <div>
+                            <textarea
+                                name=""
+                                id="" placeholder="Next Goal..."
+                                className="px-4 pt-[0.35rem] ml-[50px] mt-[10px] text-[20px] w-[90%] h-[120px] block text-gray-900 bg-gray-50 rounded-[18px] border border-gray-300 resize-none scrollbar-hide focus:outline-none"
+                            ></textarea>
+                        </div>
+                    </div>
+                    <div className="grid grid-cols-3 text-start my-10 w-max">
+                        <div className="pl-6 py-5"><span>The project is now complete and</span></div>
+                        <div className="" >
+                            <textarea
+                                name=""
+                                id="" placeholder="0-100"
+                                className="px-4 pt-[0.35rem] mt-[10px] mr-[0px] text-[20px] w-[100%] h-[45px] block text-gray-900 bg-gray-50 rounded-[18px] border border-gray-300 resize-none scrollbar-hide focus:outline-none"
+                            >
+                            </textarea>
+                        </div>
+                        <div className="pl-6 py-5"><span>Percentage</span></div>
+                    </div>
+
+                    
+                    <div className="pt-20 pr-2 grid grid-cols-12 ">
+                        <button
+                            type="button"
+                            className="col-start-12 text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:outline-none  dark:focus:ring-red-800 font-medium rounded-[18px] text-sm px-6 py-2.5 text-center mr-2 mb-2"
+                            onClick={''}
+                        >
+                            Confirm
+                        </button>
+                    </div>
                 </form>
             </div>
         </>
