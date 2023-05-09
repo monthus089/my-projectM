1505d086dde52841e75ce67f548de170214a5d4f
Monthat Muensaeng <91974247+monthus089@users.noreply.github.com>
2023-05-09 19:08:15 +0700

    new

--- src/components/MainAdminNavbar.js
+++ src/components/MainAdminNavbar.js
@@ -12,12 +12,16 @@ import { NavLink } from "react-router-dom";
 
 import logo from "../img/36-icon.png";
 import AuthContext from "./Auth/AuthProvider";
+import ChangePass from "./ChangePass";
 
 const MainLayoutAdmin = () => {
   const { user, logout } = useContext(AuthContext);
   const [open] = useState(true);
   const [isOpen, setIsOpen] = useState(false);
   const [isOpenPass, setIsOpenPass] = useState(false);
+  const closeModal = () => {
+    setIsOpenPass(false); 
+  };
   const Menu = [
     {
       title: "Project Board",
@@ -34,9 +38,6 @@ const MainLayoutAdmin = () => {
     }
   };
 
-  const handleDrop = () => {
-    console.log("handle");
-  };
 
   return (
     <>
@@ -124,96 +125,7 @@ const MainLayoutAdmin = () => {
             </div>
           </div>
           {isOpenPass ? (
-            <div
-              className="fixed z-10 inset-0 overflow-y-auto"
-              aria-labelledby="modal-title"
-              role="dialog"
-              aria-modal="true"
-            >
-              <div className="flex items-center justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0 ">
-                <div
-                  className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"
-                  aria-hidden="true"
-                ></div>
-
-                <span
-                  className="hidden sm:inline-block sm:align-middle sm:h-screen"
-                  aria-hidden="true"
-                >
-                  &#8203;
-                </span>
-
-                <div className="inline-block align-bottom bg-white rounded-[10px] text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
-                  <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
-                    <button
-                      type="button"
-                      className="text-white hover:bg-red-600  bg-gradient-to-r from-red-400 via-red-500 to-red-600 focus:outline-none border rounded-[10px] px-1 py-1 sm:ml-3 sm:w-auto sm:text-sm"
-                      data-dismiss="modal"
-                      onClick={() => setIsOpenPass((prev) => !prev)}
-                    >
-                      Close
-                    </button>
-                    <h3
-                      className="text-lg leading-6 font-medium text-gray-900 left-0"
-                      id="modal-title"
-                    >
-                      New Password
-                    </h3>
-                  </div>
-                  <div className="px-4 py-5 sm:p-6">
-                    <form>
-                      <div className="mb-4">
-                        <label
-                          htmlFor="recipient-name"
-                          className="block text-gray-700 font-bold mb-2"
-                        >
-                          Current Password:
-                        </label>
-                        <input
-                          type="password"
-                          className="form-input mt-1 block w-full rounded-[10px] border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-                          id="recipient-name"
-                        />
-                      </div>
-                      <div className="mb-4">
-                        <label
-                          htmlFor="recipient-name"
-                          className="block text-gray-700 font-bold mb-2"
-                        >
-                          New Password:
-                        </label>
-                        <input
-                          type="password"
-                          className="form-input mt-1 block w-full rounded-[10px] border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-                          id="recipient-name"
-                        />
-                      </div>
-                      <div className="mb-4">
-                        <label
-                          htmlFor="recipient-name"
-                          className="block text-gray-700 font-bold mb-2"
-                        >
-                          Confirm New Password:
-                        </label>
-                        <input
-                          type="password"
-                          className="form-input mt-1 block w-full rounded-[10px] border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-                          id="recipient-name"
-                        />
-                      </div>
-                    </form>
-                  </div>
-                  <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
-                    <button
-                      type="button"
-                      className="bg-gradient-to-r from-blue-400 via-blue-500  to-blue-600 rounded-md text-white hover:bg-blue-600 focus:outline-none border rounded-[10px] px-2 py-2 sm:ml-3 sm:w-auto sm:text-sm"
-                    >
-                      Change Password
-                    </button>
-                  </div>
-                </div>
-              </div>
-            </div>
+           <ChangePass onClose={closeModal}/>
           ) : null}
 
           <Outlet />
